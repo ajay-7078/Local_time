bd35ebb7a961f9cd7a5ba7a868e0a33516715195
ajay-7078 <ajayk889087@gmail.com>
2025-12-30 11:37:18 +0530

    update file

--- scritps3.js
+++ scritps3.js
@@ -1,13 +1,20 @@
- const clock =  document.getElementById('clock')
 
+function updateTime(){
+    document.getElementById("india-date").innerText = new Date().toLocaleDateString("en-US",{timeZone:"Asia/Kolkata", weekday:"long", year:"numeric", month:"long", day:"numeric"});
+    document.getElementById("usa-date").innerText = new Date().toLocaleDateString("en-US",{timeZone:"America/New_York", weekday:"long", year:"numeric", month:"long", day:"numeric"});
+    document.getElementById("uk-date").innerText = new Date().toLocaleDateString("en-US",{timeZone:"Europe/London", weekday:"long", year:"numeric", month:"long", day:"numeric"});
+    document.getElementById("japan-date").innerText = new Date().toLocaleDateString("en-US",{timeZone:"Asia/Tokyo", weekday:"long", year:"numeric", month:"long", day:"numeric"});
 
- let date = new Date()
- console.log(date.toLocaleDateString());
+    document.getElementById("india").innerText = new Date().toLocaleTimeString("en-US",{timeZone:"Asia/Kolkata"});
+    document.getElementById("usa").innerText = new Date().toLocaleTimeString("en-US",{timeZone:"America/New_York"});
+    document.getElementById("uk").innerText = new Date().toLocaleTimeString("en-US",{timeZone:"Europe/London"});
+    document.getElementById("japan").innerText = new Date().toLocaleTimeString("en-US",{timeZone:"Asia/Tokyo"});
+}{
+    document.getElementById("india").innerText = new Date().toLocaleTimeString("en-US",{timeZone:"Asia/Kolkata"});
+    document.getElementById("usa").innerText = new Date().toLocaleTimeString("en-US",{timeZone:"America/New_York"});
+    document.getElementById("uk").innerText = new Date().toLocaleTimeString("en-US",{timeZone:"Europe/London"});
+    document.getElementById("japan").innerText = new Date().toLocaleTimeString("en-US",{timeZone:"Asia/Tokyo"});
+}
+setInterval(updateTime,1000);
+updateTime();
 
-
- setInterval(function(){
-    
- let date = new Date()
-// console.log(date.toLocaleDateString());
-clock.innerHTML = date.toLocaleDateString();
- },1000)
